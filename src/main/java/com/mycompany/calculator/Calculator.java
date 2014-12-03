5fa94a27e5a97b2ffa47c25edaee69156e4e5f90
KonstGit <kostyan.vk@gmail.com>
2014-12-03 23:35:33 +0200

    1

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Konst
+ */
+public class Calculator {
+    
+    List<Operation> list = new ArrayList<>();
+    
+    public Calculator() {
+        list.add(new Addition());
+        list.add(new Subtracting());
+        list.add(new Multiplication());
+        list.add(new Division());
+    }
+    
+    public String[] getOperations() {
+        String[] operations = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            operations[i] = list.get(i).getOperationSign();
+            
+        }
+        return operations;
+    }
+    
+    public Double perform(String op, Double a, Double b) throws Exception {
+        for (Operation o : list) {
+            if (o.getOperationSign().equals(op)) {
+                return o.perform(a, b);
+            }
+        }
+        return null;// throw new Exception("EXCEPTION: Operation not found");
+    }
+}
---
+++ src/main/java/com/mycompany/calculator/CalculatorConsoleApp.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Konst
+ */
+public class CalculatorConsoleApp {
+
+    static Calculator calculator = new Calculator();
+    static String operation = null;
+
+    public static void main(String[] args) throws Exception {
+
+        do {
+            Scanner sc = new Scanner(System.in);
+            // greatings
+            System.out.println("*** Greetings ***");
+
+            // print operations
+            System.out.println("Available operations:");
+
+            for (String op : calculator.getOperations()) {
+                System.out.print(op);
+                System.out.print(" ");
+            }
+            //------------------------------------------------------------
+
+            // check operation sign
+            while (!checkOperation(sc.nextLine())) {
+                System.out.println("There isn't such operation");
+            }
+            //Read operation
+            if ("exit".equalsIgnoreCase(operation)) {
+                System.out.println("See you!");
+                break;
+            }
+
+            //Read values, perform operation, write it 
+            // Read first value
+            System.out.println("Input first value");
+            Double firstValue;
+            while (!sc.hasNextDouble()) {
+                System.out.println("only Double");
+                sc.next();
+            }
+            firstValue = sc.nextDouble();
+
+            // Read second value
+            System.out.println("Input second value");
+            Double secondValue;
+            while (!sc.hasNextDouble()) {
+                System.out.println("only Double");
+                sc.next();
+            }
+            secondValue = sc.nextDouble();
+
+            //Perform operation
+            System.out.print("\n "
+                    + firstValue + " "
+                    + operation + " "
+                    + secondValue + " = "
+                    + calculator.perform(operation, firstValue, secondValue) + "\n");
+
+            sc.reset();
+            System.out.println("-------------------------");
+        } while (true);
+    }
+
+    private static boolean checkOperation(String op) {
+
+        for (String st : calculator.getOperations()) {
+            if (op.equals(st) || op.equals("exit")) {
+                operation = op;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
