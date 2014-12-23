b8a7654a80459623c3c66aeb0755cb52c37163cc
Конст <@10.42.12.94>
2014-12-23 10:46:30 +0200

    + reflect

---
+++ src/main/java/com/mycompany/calculator/operations/Addition.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculator.operations;
+
+import com.mycompany.calculator.Operation;
+
+/**
+ *
+ * @author Konst
+ */
+public class Addition implements Operation {
+
+    @Override
+    public Double perform(Double a, Double b) {
+        return a + b;
+    }
+
+    @Override
+    public String getOperationSign() {
+        return "+";
+    }
+
+}
